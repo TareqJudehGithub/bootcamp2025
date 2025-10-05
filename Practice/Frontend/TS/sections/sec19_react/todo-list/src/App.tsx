62f39f5a37490227892d12c68f2f16565cb125d5
TareqJudehGithub <tareq.joudeh@gmail.com>
2025-10-05 14:39:36 +0300

    Completed section 19: TS and React.

--- Practice/Frontend/TS/sections/sec19_react/todo-list/src/App.tsx
+++ Practice/Frontend/TS/sections/sec19_react/todo-list/src/App.tsx
@@ -1,22 +1,37 @@
+// React
 import { useState } from "react";
 
+// Components
 import ShoppingList from "./components/ShoppingList";
 import ShoppingListForm from "./components/ShoppingListForm";
 
+// Data
 import Product from "./components/models/products";
 
+// Styling
 import "./App.css";
 
 function App() {
-	const items = [
+	const items: Product[] = [
 		{ id: 1, product: "Lemon", quantity: 50, description: "Fresh lemon" },
 		{ id: 2, product: "Apples", quantity: 7, description: "Fresh apples" },
 	];
-	const [product, setProduct] = useState<Product[]>(items);
+	const [products, setProducts] = useState<Product[]>(items);
+
+	// Handlers
+	function handleAddItem(item: Product) {
+		setProducts((items) => [...items, item]);
+	}
+
+	const addItem = (product: Product) => {
+		console.log("Make it to the App component!");
+		setProducts((products) => [product, ...products]);
+	};
+
 	return (
 		<div className="">
-			<ShoppingList products={product} />
-			<ShoppingListForm />
+			<ShoppingList products={products} />
+			<ShoppingListForm products={products} onAddProduct={addItem} />
 		</div>
 	);
 }
--- Practice/Frontend/TS/sections/sec19_react/todo-list/src/components/ShoppingListForm.tsx
+++ Practice/Frontend/TS/sections/sec19_react/todo-list/src/components/ShoppingListForm.tsx
@@ -1,16 +1,55 @@
-import React, { JSX } from "react";
+import React, { JSX, useRef, useState } from "react";
+import Product from "./models/products";
 
-function ShoppingListForm(): JSX.Element {
+function ShoppingListForm(props: ShoppingListFormProps): JSX.Element {
+	const inputRef = useRef<HTMLInputElement>(null);
+	const [product, setProduct] = useState<string>("");
+	const [quantity, setQuantity] = useState<number>(0);
+	const [description, setDescription] = useState<string>("");
+
 	// Handles
 	function handleSubmit(e: React.FormEvent) {
 		e.preventDefault();
-		console.log("Form submitted Successfully!");
+
+		// const newProduct = inputRef.current?.value;
+		// props.onAddProduct(newProduct);
+		// inputRef.current.value = "";
+
+		const newProduct: Product = {
+			id: props.products.length + 1,
+			product: product,
+			quantity: quantity,
+			description: description,
+		};
+		props.onAddProduct(newProduct);
 	}
 	return (
 		<form onSubmit={handleSubmit}>
-			<input type="text" placeholder="Product name.." />
+			<input
+				type="text"
+				placeholder="Product name.."
+				value={product}
+				onChange={(e) => setProduct(e.target.value)}
+			/>
+			<input
+				type="number"
+				placeholder="Qty.."
+				value={quantity}
+				onChange={(e) => setQuantity(Number(e.target.value))}
+			/>
+			<input
+				type="text"
+				placeholder="Product Description.."
+				value={description}
+				onChange={(e) => setDescription(e.target.value)}
+			/>
 			<button type="submit">Add Item</button>
 		</form>
 	);
 }
 export default ShoppingListForm;
+
+interface ShoppingListFormProps {
+	products: Product[];
+	onAddProduct: (item: Product) => void;
+}
