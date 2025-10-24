b0547947f6b80e9bef5ffc5ace6efc8142e4085a
TareqJudehGithub <tareq.joudeh@gmail.com>
2025-10-24 22:44:11 +0300

    Implemented Configure class, and migrated both Teams and League entities with seeded data into MS SQL server succeffully.

--- Practice/Backend/EF_Core/db_scripts/Football_League_EFCore_CREATE.sql
+++ Practice/Backend/EF_Core/db_scripts/Football_League_EFCore_CREATE.sql
@@ -1,9 +1,46 @@
+-- Server full name: TareqPC\MSSQLSRV
+
+-- EF Course DB
 CREATE DATABASE FootballLeague_EFCore
 GO
-
 USE FootballLeague_EFCore
+GO
+
+-- Practice DB
+CREATE DATABASE FootballLeague_Practice
+GO
+USE FootballLeague_Practice
 GO
 
 SELECT *
 FROM dbo.Teams
 GO
+
+SELECT *
+FROM dbo.Coaches
+GO
+
+
+DELETE
+FROM dbo.Coaches
+WHERE Id = 5;
+
+INSERT INTO dbo.Teams (Name, CreatedDate)
+VALUES 
+('Real Madrid', GETDATE()),
+('Barcelona', GETDATE()),
+('AC Milan', GETDATE()),
+('Inter Milan', GETDATE()),
+('Juventus', GETDATE()),
+('PSG', GETDATE())
+GO
+
+
+INSERT INTO dbo.Teams 
+VALUES
+('AC Monaco', GETDATE()),
+('AC Flamengo', GETDATE())
+GO
+
+
+
