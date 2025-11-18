18aa822e5d0388f4fc65515372a6f5c2c6dba02d
TareqJudehGithub <tareq.joudeh@gmail.com>
2025-11-19 00:29:59 +0300

    pushed to CSharpMasterClass and LINQ repos, the Loop module and exercises.

---
+++ Practice/LINQ/Sec2Introduction/Program.cs
@@ -0,0 +1,28 @@
+﻿// Write a program that returns true if any item in words list is uppercase.
+List<string> words = ["quick", "BROWN", "fox"];
+
+
+// Old school C#
+bool IsAnyWordUppercase(List<string> listOfWords)
+{
+    bool result = false;
+    foreach (var word in listOfWords)
+    {
+        if (word == word.ToUpper())
+        //Console.WriteLine(word.ToUpper());
+        {
+            return result = true;
+        }
+    }
+    return result;
+}
+bool isAnyWordUppercase = IsAnyWordUppercase(words);
+Console.WriteLine(isAnyWordUppercase);
+
+
+bool isUpper(List<string> wordsList)
+{
+    return wordsList.Any(word => word == word.ToUpper());
+}
+
+Console.WriteLine(isUpper(words));
